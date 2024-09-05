961e30059ab89b4269cd1cbf7bfdc22bed738158
João Pedro Giacometti Guerreiro <j.guerreiro@unesp.br>
2024-09-05 10:17:29 -0300

    feat: add CI

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -19,14 +19,14 @@ export async function generateMetadata({
   };
 }
 
-export async function generateStaticParams() {
-  const response = await api("/products/featured");
-  const products: Product[] = await response.json();
+// export async function generateStaticParams() {
+//   const response = await api("/products/featured");
+//   const products: Product[] = await response.json();
 
-  return products.map((product) => {
-    return { slug: product.slug };
-  });
-}
+//   return products.map((product) => {
+//     return { slug: product.slug };
+//   });
+// }
 
 async function getProductBySlug(slug: string): Promise<Product> {
   const response = await api(`/products/${slug}`, {
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { CartWidget } from "./cart-widget";
 import { SearchForm } from "./search-form";
+import { Suspense } from "react";
 
 export const Header = () => {
   return (
@@ -10,8 +11,9 @@ export const Header = () => {
         <Link href="/" className="text-2xl font-extrabold text-white">
           Devstore
         </Link>
-
-        <SearchForm />
+        <Suspense fallback={null}>
+          <SearchForm />
+        </Suspense>
       </div>
       <div className="flex items-center gap-4">
         <CartWidget />
